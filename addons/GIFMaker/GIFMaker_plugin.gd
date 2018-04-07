7b6a42fbdcb28e2374b406447844c4903e539c87
Henrique L Alves <henriquelalves@gmail.com>
2018-04-07 00:36:52 -0300

    Refactored

--- addons/GIFMaker/GIFMaker_plugin.gd
+++ addons/GIFMaker/GIFMaker_plugin.gd
@@ -1,10 +1,15 @@
+#MADE BY HENRIQUE ALVES
+#MIT LICENSE
+#STUFF
+#YADA YADA
+
 tool
 extends EditorPlugin
 
 func _enter_tree():
 	# When this plugin node enters tree, add the custom type
 
-	add_custom_type("GIFMaker","ReferenceFrame",preload("res://addons/GIFMaker/GIFMaker.gd"),preload("res://addons/GIFMaker/GIFMaker_icon.png"))
+	add_custom_type("GIFMaker","ReferenceRect",preload("res://addons/GIFMaker/GIFMaker.gd"),preload("res://addons/GIFMaker/GIFMaker_icon.png"))
 
 func _exit_tree():
 	# When the plugin node exits the tree, remove the custom type
---
+++ addons/GIFMaker/kdtree.gd
@@ -0,0 +1,12 @@
+#MADE BY HENRIQUE ALVES
+#MIT LICENSE
+#STUFF
+#YADA YADA
+
+var _data = []
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
